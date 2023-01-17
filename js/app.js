e956d49e56a39fa9777919032675f690fc81ed6d
Jose Mendoza <jose.miguel.mendoza.molina@gmail.com>
2023-01-17 17:24:00 -0300

    Faltando un metodo de busqueda y filtrado, el que use es a mano. (chequeoMail)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-
-function ingreso_nuevo_usuario(){
+function ingreso_usuario(){
     const lista_usuarios =[]
 
     function Usuario(nombre, apellido, edad, email, pais_origen){
@@ -18,40 +17,80 @@ function ingreso_nuevo_usuario(){
         let e_mail = prompt("Ingrese su dirección de correo electrónico: ")
         let pais = prompt("Ingrese su país de origen: ")
 
-        const usuario_ingresado = new Usuario(nombre, apellido, edad, e_mail, pais)
+        let usuario_ingresado = new Usuario(nombre, apellido, edad, e_mail, pais)
         lista_usuarios.push(usuario_ingresado)
         console.log(lista_usuarios)
+        return lista_usuarios;
     } 
     else{
-        alert("De acuerdo, que tenga buen día.")
+        if (bienvenida == 'NO' || bienvenida == 'no'){
+            alert("Vuelva a intentarlo.")
+            ingreso_usuario();
+        }
+        else{
+            alert("Opción no valida.")
+            ingreso_usuario();
+        }
     }
 }
 
-function seguros(){
+let ingreso = ingreso_usuario()
+
+function chequeoMail(lista_usuario){
+    let y
+    let indice = 0
+    let mail_asegurado = prompt("Ingrese su mail nuevamente: ")
+    for(y = 0; y < lista_usuario.length; y++){
+        if (lista_usuario[y].email.includes(mail_asegurado) == true ){
+            indice = lista_usuario[y].email.indexOf(mail_asegurado)
+            return indice
+        }
+        else{
+            alert("Este mail no se encuentra registrado.")
+            seguros(lista_usuario)
+        }
+    }
+}
+
+function seguros(lista_usuario){
+    const IVA = 1.21
     let tipo
     do{
         tipo = prompt("¿Qué tipo de seguro está buscando?\n1 - Seguro de Hogar\n2 - Seguro de Vida\n3 - Seguro médico\n4 - Salir")
         tipo = parseInt(tipo)
         switch (tipo){
             case 1:
+                let indice_user = chequeoMail(lista_usuario)
                 let opcion = prompt("En este campo, le haremos preguntas sobre cuanto valor tienen sus pertenencias, si esta inseguro de esto, asesorese en manos de expertos en el tema.\n¿Desea volver? (YES/NO)")
                 if(opcion == 'YES' || opcion == 'yes'){
-                    seguros()
+                    seguros(lista_usuario)
                 }
                 else{
                     if(opcion == 'NO' || opcion == 'no'){
-                        // Quiero que NOMBR NO EXISTA MAS, ya que escribimos nuestro nombre al inicio del simulador.
-                        nombr = prompt("Ingrese su nombre: ")
-                        console.log("El nombre de la persona que quiere un seguro de hogar es"+" "+nombr) 
+                            console.log("El nombre de la persona que quiere un seguro de hogar es "+ lista_usuario[indice_user].nombre) 
                         alert("Le indicaremos a continuación los pasos a realizar.\n1 - Hacer un inventario de las cosas de la casa. (Mobiliario, electrodomesticos y dispositivos, enseres y artículos decorativos, joyas y objetos de valor).")
                         let valor_pertenencias = prompt("2 - Calcular el valor de todas sus pertenencias ya antes mencionadas en el paso 1. ¿De cuanto estamos hablando? Por favor, ingrese la suma total, sin tener en cuenta las joyas y objetos de valor especial.")
                         valor_pertenencias = parseFloat(valor_pertenencias)
                         console.log("El valor total de sus pertenencias es de"+" "+valor_pertenencias)
                         alert("3 - Valore sus joyas y objetos de valor especial, es recomendable hacer esto ya que varias aseguradoras valoras estos objetos de forma diferente a precios distintos. Todo objeto de valor o joya debe superar los 2000 euros.")
-                        // Quiero que CORREO NO EXISTA MAS, ya que escribimos nuestro correo al inicio del simulador.
-                        correo = prompt("Ingrese su mail para enviarle los datos y pasos a continuación: ")
-                        console.log("Su correo es:"+" "+correo)
-                        alert("¡Correo enviado! Gracias por elegirnos.")
+                        let valorJoyas = prompt("4 - Entonces, ¿Cuanto sería el total teniendo en cuenta las joyas, y objetos de valor especial?")
+                        console.log("El valor total de su joyería y demás objetos valiosos es de "+valorJoyas)
+                        let totalPertenencias = (valor_pertenencias + valorJoyas) * IVA / 10000
+                        let acuerdo = prompt("Su total sería de "+ totalPertenencias+". ¿Esta de acuerdo? (YES/NO): ")
+                        if (acuerdo == 'YES' || acuerdo == 'yes'){
+                            console.log("Su correo es: "+ lista_usuario[indice_user].email)
+                            alert("¡Correo enviado!\nRevise su mail para revisar que todo este en orden, y asi luego seguir los pasos a continuación. Gracias por elegirnos, hasta pronto.")
+                            console.log("Se le ha enviado un mail a "+ lista_usuario[indice_user].nombre+" habiendo aceptado el acuerdo.")
+                        }
+                        else{ 
+                            if (acuerdo =='NO' || acuerdo == 'no'){
+                            alert("De acuerdo. Si le interesa, ¡puede volver a comunicarse con nosotros!")
+                            console.log(lista_usuario[indice_user].nombre+" no estuvo de acuerdo con el precio.")
+                            }
+                            else{
+                                alert("Opción no valida, intente de nuevo.")
+                            }
+                        }
                     }
                     else{
                         alert("Opción invalida. Por favor, reintente el simulador.")
@@ -59,40 +98,43 @@ function seguros(){
                 }
                 break
             case 2:
+                let indice_usuario = chequeoMail(lista_usuario);
                 let capital = prompt("1 - Ingrese la capital a recibir: ")
                 capital = parseFloat(capital)
                 console.log("La capital a recibir es de"+" "+capital)
-                //Quiero que "nombre" tampoco exista aca, ya que escribimos el nombre al inicio del simulador.
-                nombre = prompt("3 - Ingrese su nombre completo, por favor: ")
-                console.log("La persona que quiere adquirir el seguro de vida se llama"+" "+nombre)
-                // Lo mismo acá con mail.
-                mail = prompt("4 - Ya calculamos su seguro de vida, por favor ingrese su mail a continuación para enviarle los calculos ya realizados: ")
-                console.log("Su mail es:"+" "+mail)
-                alert("¡Gracias!\nQue tenga buen día, adios.")
+                console.log("La persona que quiere adquirir el seguro de vida se llama "+lista_usuario[indice_usuario].nombre)
+                let servicio_vida = capital * lista_usuario[indice_usuario].edad / 30
+                let afirmacion = prompt("El valor de su seguro de vida total, contando la antiguedad y la tasa de mortalidad es: "+ servicio_vida+". ¿Está de acuerdo? (YES/NO)")
+                if (afirmacion == 'YES' || afirmacion == 'yes'){
+                    console.log("Su mail es: "+lista_usuario[indice_usuario].email)
+                    alert("¡Gracias!\nLe enviaremos un mail con los datos confirmados. El mail podría llegar entre un periodo de 48hs. Que tenga buen día, adios.")
+                }
+                else{
+                    if (afirmacion == 'no' || afirmacion == 'NO'){
+                        console.log(lista_usuario[0].nombre+" ha negado el acuerdo.")
+                    }
+                }
                 break
             case 3:
+                let indi_user = chequeoMail(lista_usuario);
                 let poliza = prompt("¿Ya tiene una poliza de salud? (SI/NO): ")
                 if(poliza == 'SI' || poliza == 'si' || poliza == 'NO' || poliza == 'no'){
-                    console.log("Esta persona"+" "+poliza+" "+"tiene una poliza de salud.")
-                    // Aqui no es necesario.
-                    let primer_nombre_asegurado = prompt("Ingrese su nombre: ")
-                    console.log("El nombre del primer asegurado es"+" "+primer_nombre_asegurado)
-                    let primer_asegurado = prompt("Por favor, ingrese su fecha de nacimiento sin espaciados ni guiones (DD/MM/AA): ")
-                    primer_asegurado = parseInt(primer_asegurado)
-                    console.log("La fecha de nacimiento de"+" "+primer_nombre_asegurado+" "+"es"+" "+primer_asegurado)
+                    console.log(lista_usuario[indi_user].nombre+" "+poliza+" "+"tiene una poliza de salud.")
+                    let primer_asegurado_fecha = prompt("Por favor, ingrese su fecha de nacimiento sin espaciados ni guiones (DD/MM/AA): ")
+                    primer_asegurado = parseInt(primer_asegurado_fecha)
+                    console.log("La fecha de nacimiento de"+" "+lista_usuario[indi_user].nombre+" "+"es"+" "+primer_asegurado_fecha)
                     sexo = prompt("Sexo femenino o másculino (F/M): ")
                     if (sexo == 'F' || sexo == 'M'){
-                        console.log("El género de esta persona es"+" "+sexo)
+                        console.log("El género de "+lista_usuario[indi_user].nombre+" es "+sexo)
                         let varios_asegurados = prompt("¿Desea agregar a alguien más? (SI/NO)")
-                        if (varios_asegurados == 'NO'){
-                            email = prompt("Ingrese su mail a continuación para enviarle los datos: ")
-                            console.log("El mail a enviar todos los datos es:"+" "+email)
+                        if (varios_asegurados == 'NO' || varios_asegurados == 'no'){
+                            console.log("El mail a enviar todos los datos es:"+" "+lista_usuario[indi_user].email)
                             alert("Le enviamos un mail con los siguientes pasos a realizar, verifique si lo ha recibido y notifiquenos. ¡Que tenga buen día!")
                         }
                         else{
                             if(varios_asegurados == 'SI' || varios_asegurados == 'si'){
                                 while(varios_asegurados != 'NO'){
-                                    console.log(primer_nombre_asegurado+" "+"decidió agregar a alguien más.")
+                                    console.log(lista_usuario[indi_user].nombre+" "+lista_usuario[indi_user].apellido+" decidió agregar a alguien más.")
                                     let nombre_asegurado = prompt("Ingrese el nombre del asegurado: ")
                                     console.log("El nombre del siguiente asegurado es"+" "+nombre_asegurado)
                                     let segundo_asegurado = prompt("Por favor, ingrese la fecha de nacimiento del asegurado (DD/MM/AA): ")
@@ -102,8 +144,7 @@ function seguros(){
                                     console.log("Y su género es"+" "+sexo)
                                     varios_asegurados = prompt("¿Desea agregar a alguien más? (SI/NO)")
                                 }
-                                email = prompt("Ingrese su mail a continuación para enviarle los datos: ")
-                                console.log("El email a enviar todos los datos es:"+" "+email)
+                                console.log("El email a enviar todos los datos es: "+lista_usuario[indi_user].email)
                                 alert("Le enviamos un mail con los siguientes pasos a realizar, verifique si lo ha recibido y notifiquenos. ¡Que tenga buen día!")
                             }
                             else{
@@ -128,8 +169,7 @@ function seguros(){
     }while (tipo != 4)
 }
 
-ingreso_nuevo_usuario();
-seguros();
+seguros(ingreso);
 
 
 
